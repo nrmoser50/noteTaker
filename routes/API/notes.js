7be6f6fe2ec38ab6361c1024cd6fa953eecf6c94
Nicholas Moser <nicholas.r.moser@gmail.com>
2023-01-22 22:10:05 -0700

    updated notes.js and server.js

--- routes/API/notes.js
+++ routes/API/notes.js
@@ -3,6 +3,7 @@ const fs = require("fs");
 const util = require("util");
 const readFile = util.promisify(fs.readFile);
 const writeFile = util.promisify(fs.writeFile);
+const { v4: uuidy4 } = require('uuid');
 
 const getNotes = () => {
   return readFile("db/db.json", "utf-8").then((rawNotes) =>
@@ -13,5 +14,31 @@ const getNotes = () => {
 router.get("/", (req, res) => {
   getNotes().then((notes) => res.json(notes));
 });
+
+router.post("/", (req, res) => {
+  const { title, text } = req.body;
+  const db = JSON.parse(fs.readFileSync('./db/db.json', (err) => {
+    if (err) throw err;
+    const item = {
+      id: uuidy4(),
+      title,
+      text,
+    }
+    db.push(item);
+    fs.writeFileSync('./db/db.json', JSON.stringify(db, null, 2));
+    }))
+  });
+
+
+router.delete("/", (req, res) => {
+  const db = JSON.parse(fs.readFileSync('../db/db.json', (err) => {
+    if (err) console.log(err);
+    const newDb = db.filter((note) => {
+      return note.id !== req.params.id;
+    })
+    fs.writeFileSync('../db/db.json', JSON.stringify(newDb, null, 2));
+    res.json({message: `Deleted ${req.params.id}`})
+   }))
+  });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const PORT = 3001;
 const routes = require("./routes");
+const fs = require("fs");
+const { randomUUID } = require("crypto");
 
 
 app.use(express.json());
@@ -9,7 +11,36 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(routes);
 
+app.get('/notes', (res, req) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
 
+app.get('*', (res, req) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.get('/api/notes', (res, req) => {
+
+    fs.readFile('db/db.json', 'utf-8', (err, data) => {
+        if (err) throw err;
+        parsedNotes = JSON.parse(data);
+        res.json(parsedNotes);
+    })
+})
+
+// app.post('/api/notes', (req, res) => {
+//     const { title, text } = req.body;
+//     const db = JSON.parse(fs.readFileSync('./db/notes.json', (err) => {
+//         if (err) throw err;
+    
+//     const newNote = {
+//         title,
+//         text,
+//         id: randomUUID()
+//     }
+//     db.push(newNote);
+// }))
+// })
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
